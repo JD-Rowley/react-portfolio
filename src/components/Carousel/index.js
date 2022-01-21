b7a192c9a2a623c13aa193c12c45f6a734599aef
JD Rowley <jdrowley1989@gmail.com>
2022-01-21 14:04:12 -0700

    began carousel work

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+import { FaChevronLeft } from 'react-icons/fa';
+import { FaChevronRight } from 'react-icons/fa';
+import './index.css';
+
+function Carousel(props) {
+    return (
+        <div className='flex-container'>
+            <div className='carousel'>
+                <a className='fas fa-chevron-left slide-btn prev'><FaChevronLeft /></a>
+                <div className='carousel-container'>
+                    <div className='slides'>
+                        <div id='feature-el' className='slide fade'>
+                            <img src={props.image} className='slide-img' alt={props.title} key={props.title} />
+                            <a href={props.deploy} target='_blank' rel='noreferrer' className='title project'><h3>{props.title} {props.deploy !== null && <FaExternalLinkAlt />}</h3></a>
+                            <ul>
+                                <li><b>GitHub:</b> <a href={props.github_url}>{props.github}</a></li>
+                                <li><b>Description: </b>{props.description}</li>
+                                <li><b>Languages/Tools: </b>{props.languages}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <a className='fas fa-chevron-right slide-btn next'><FaChevronRight /></a>
+            </div>
+        </div>
+    );
+}
+
+export default Carousel;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import './index.css';
+import Carousel from '../Carousel';
 import ProjectCard from '../ProjectCard';
-import projects from '../projects.js'
+import projects from '../projects.js';
+import './index.css';
 
 function Project() {
     return(
         <section className="container">
+            {projects.slice(0, 3).map(project => (
+                <Carousel 
+                    key={project.title}
+                    title={project.title}
+                    deploy={project.deploy}
+                    github_url={project.github_url}
+                    github={project.github}
+                    description={project.description}
+                    languages={project.languages}
+                    image={project.image}
+                />            
+            ))}
             <div className="other-projects">
             <h2>Projects:</h2>
             </div>
             <div className="projects-container">
-                {projects.map(project => (
+                {projects.slice(3, projects.length).map(project => (
                     <ProjectCard
                         key={project.title}
                         title={project.title}
