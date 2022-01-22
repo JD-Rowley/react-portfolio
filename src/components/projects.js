3c8a80402a8581acfc9c452cd6f719b9dd81e79c
JD Rowley <jdrowley1989@gmail.com>
2022-01-22 16:41:55 -0700

    added projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,6 +11,8 @@ import runBuddy from '../assets/project_img/run-buddy.JPG';
 import codeQuiz from '../assets/project_img/code-quiz.JPG';
 import passwordGenerator from '../assets/project_img/password-generator.JPG';
 import deepThoughts from '../assets/project_img/deep-thoughts.JPG';
+import budgetTracker from '../assets/project_img/budget-tracker.JPG';
+import readmeGenerator from '../assets/project_img/readme-generator.JPG';
 
 const projects =
 
@@ -34,15 +36,6 @@ const projects =
         "image": dogDossier
     },
     {
-        "title": "LocalGrub",
-        "deploy": "https://gelenhaus.github.io/groupProject1/",
-        "github_url": "https://github.com/Gelenhaus/groupProject1",
-        "github": "LocalGrub",
-        "description": "A website designed to cloak domestic violence shelter searches and user requests for 911 assistance.",
-        "languages": "HTML, CSS, Javascript",
-        "image": LocalGrub
-    },
-    {
         "title": "Tech Blog",
         "deploy": "https://cryptic-bastion-77900.herokuapp.com/",
         "github_url": "https://github.com/JD-Rowley/mvc-tech-blog",
@@ -52,13 +45,49 @@ const projects =
         "image": techBlog
     },
     {
-        "title": " E-Commerce Backend",
-        "deploy": null,
-        "github_url": "https://github.com/JD-Rowley/e-commerce-backend",
-        "github": "e-commerce-backend",
-        "description": "A working backend for an E-Commerce website.",
-        "languages": "Javascript, Express.js, Sequelize, Node.js",
-        "image": eCommerce
+        "title": "LocalGrub",
+        "deploy": "https://gelenhaus.github.io/groupProject1/",
+        "github_url": "https://github.com/Gelenhaus/groupProject1",
+        "github": "LocalGrub",
+        "description": "A website designed to cloak domestic violence shelter searches and user requests for 911 assistance.",
+        "languages": "HTML, CSS, Javascript",
+        "image": LocalGrub
+    },
+    {
+        "title": "Budget Tracker",
+        "deploy": "https://polar-tor-80486.herokuapp.com/",
+        "github_url": "https://github.com/JD-Rowley/budget-tracker-pwa",
+        "github": "budget-tracker-pwa",
+        "description": "A progressive web application that gives users the ability to track spending and income.",
+        "languages": "Javascript, Express.js, Mongoose, Node.js",
+        "image": budgetTracker
+    },
+    {
+        "title": "Deep Thoughts",
+        "deploy": "https://fast-eyrie-29672.herokuapp.com/",
+        "github_url": "https://github.com/JD-Rowley/deep-thoughts",
+        "github": "deep-thoughts",
+        "description": "Deep Thoughts is a social media application where users can share their own thoughts and react to others' thoughts.",
+        "languages": "HTML, CSS, Javascript, MongoDB, Express.js, React, GraphQL",
+        "image": deepThoughts
+    },
+    {
+        "title": "Weather Dashboard",
+        "deploy": "https://jd-rowley.github.io/weather-dashboard/",
+        "github_url": "https://github.com/JD-Rowley/weather-dashboard",
+        "github": "weather-dashboard",
+        "description": "An app using third-party APIs to help travelers find weather information in their area.",
+        "languages": "HTML, CSS, Javascript, APIs",
+        "image": weatherImage
+    },
+    {
+        "title": "Code Quiz",
+        "deploy": "https://jd-rowley.github.io/code-quiz/",
+        "github_url": "https://github.com/JD-Rowley/code-quiz",
+        "github": "code-quiz",
+        "description": "A webpage with a timed quiz about coding.",
+        "languages": "HTML, CSS, Javascript",
+        "image": codeQuiz
     },
     {
         "title": "Note-Taker",
@@ -68,6 +97,15 @@ const projects =
         "description": "An app for creating notes.",
         "languages": "HTML, CSS, Javascript, Express.js",
         "image": noteTaker
+    },
+    {
+        "title": "E-Commerce Backend",
+        "deploy": null,
+        "github_url": "https://github.com/JD-Rowley/e-commerce-backend",
+        "github": "e-commerce-backend",
+        "description": "A working backend for an E-Commerce website.",
+        "languages": "Javascript, Express.js, Sequelize, Node.js",
+        "image": eCommerce
     },
     {
         "title": "Social Network API",
@@ -88,13 +126,13 @@ const projects =
         "image": teamProfile
     },
     {
-        "title": "Weather Dashboard",
-        "deploy": "https://jd-rowley.github.io/weather-dashboard/",
-        "github_url": "https://github.com/JD-Rowley/weather-dashboard",
-        "github": "weather-dashboard",
-        "description": "An app using third-party APIs to help travelers find weather information in their area.",
-        "languages": "HTML, CSS, Javascript, APIs",
-        "image": weatherImage
+        "title": "ReadMe Generator",
+        "deploy": null,
+        "github_url": "https://github.com/JD-Rowley/inquirer-driven-readme-generator",
+        "github": "inquirer-driven-readme-generator",
+        "description": "A README.md file generator designed to ease the burden of creating a README file by asking the user questions and populating the sections accordingly.",
+        "languages": "Javascript, Inquirer, Node.js",
+        "image": readmeGenerator
     },
     {
         "title": "Run Buddy",
@@ -106,15 +144,6 @@ const projects =
         "image": runBuddy
     },
     {
-        "title": "Code Quiz",
-        "deploy": "https://jd-rowley.github.io/code-quiz/",
-        "github_url": "https://github.com/JD-Rowley/code-quiz",
-        "github": "code-quiz",
-        "description": "A webpage with a timed quiz about coding.",
-        "languages": "HTML, CSS, Javascript",
-        "image": codeQuiz
-    },
-    {
         "title": "Password Generator",
         "deploy": "https://jd-rowley.github.io/password-generator/",
         "github_url": "https://github.com/JD-Rowley/password-generator",
@@ -122,15 +151,6 @@ const projects =
         "description": "A webpage that accepts certain parameters to create a password.",
         "languages": "HTML, CSS, Javascript",
         "image": passwordGenerator
-    },
-    {
-        "title": "Deep Thoughts",
-        "deploy": "https://fast-eyrie-29672.herokuapp.com/",
-        "github_url": "https://github.com/JD-Rowley/deep-thoughts",
-        "github": "deep-thoughts",
-        "description": "Deep Thoughts is a social media application where users can share their own thoughts and react to others' thoughts.",
-        "languages": "HTML, CSS, Javascript, MongoDB, Express.js, React, GraphQL",
-        "image": deepThoughts
     }
 ]
 
